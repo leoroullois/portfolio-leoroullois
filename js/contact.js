307f735d2c601c779a81c9a4c8ba1de93cb3db67
leoroullois <roullois.leo@gmail.com>
2022-02-23 14:20:14 +0100

    feat: contact form working with formsubmit.co

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,36 @@
-// ? Loader
-const loader = document.querySelector("#loader");
-window.addEventListener("load", () => {
-	loader.classList.add("fondu-out");
-	setTimeout(() => {
-		loader.style.visibility = "hidden";
-		loader.remove();
-		document.querySelector("body").style.overflow = "visible";
-	}, 200);
-});
+// (function () {
+// 	// https://dashboard.emailjs.com/admin/integration
+// 	emailjs.init("user_ck7qCEQCHOoizvqKb7VzX");
+// })();
+// window.onload = function () {
+// 	document
+// 		.getElementById("contact-form")
+// 		.addEventListener("submit", function (event) {
+// 			event.preventDefault();
+// 			// generate a five digit number for the contact_number variable
+// 			this.contact_number.value = (Math.random() * 100000) | 0;
+// 			// these IDs from the previous steps
+// 			emailjs.sendForm("contact_service", "contact_form", this).then(
+// 				function () {
+// 					console.log("SUCCESS!");
+// 				},
+// 				function (error) {
+// 					console.log("FAILED...", error);
+// 				}
+// 			);
+// 		});
+// };
+
+// // ? Loader
+// const loader = document.querySelector("#loader");
+// window.addEventListener("load", () => {
+// 	loader.classList.add("fondu-out");
+// 	setTimeout(() => {
+// 		loader.style.visibility = "hidden";
+// 		loader.remove();
+// 		document.querySelector("body").style.overflow = "visible";
+// 	}, 200);
+// });
 
 // ? Menu hamburger
 const myIcon = document.querySelector("#hamburger-icon");
